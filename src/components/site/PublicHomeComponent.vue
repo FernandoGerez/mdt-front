103a4155c0c026edf5ed64877b1e090aa2d512a7
fjbalsamo <fjbalsamo@yahoo.com.ar>
2020-06-23 00:50:12 -0300

    llegamos al header

---
+++ src/components/site/PublicHomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="public-header">
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  <h2>
+                      aca va la foto del flaco con cara de puto
+                  </h2>
+              </div>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'PublicHomeComponent'
+}
+</script>
